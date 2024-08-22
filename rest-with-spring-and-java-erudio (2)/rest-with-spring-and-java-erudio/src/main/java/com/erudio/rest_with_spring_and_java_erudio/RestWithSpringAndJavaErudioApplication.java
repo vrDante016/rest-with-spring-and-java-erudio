743d6a6c776f0eb83aeb2ed4dec60c5c9b964b19
Rafael Germano Lopes de Sousa <rafael.sousa@JPFARMA.INTRANET>
2024-08-22 12:48:19 -0300

    commitado

---
+++ rest-with-spring-and-java-erudio (2)/rest-with-spring-and-java-erudio/src/main/java/com/erudio/rest_with_spring_and_java_erudio/RestWithSpringAndJavaErudioApplication.java
@@ -0,0 +1,13 @@
+package com.erudio.rest_with_spring_and_java_erudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringAndJavaErudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringAndJavaErudioApplication.class, args);
+	}
+
+}
